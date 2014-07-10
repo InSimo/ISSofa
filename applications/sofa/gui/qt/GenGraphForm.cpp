b7570a1e623f1f3043ffb6f9ce530a48128609ac
Thomas Jund <thomas.jund@insimo.fr>
2014-07-10 16:44:29 +0000

    CLEAN: reduce scope of var & remove unsused instructions
    git-svn-id: https://code.insimo.fr/svn/IS/ISSofa/branches/ISSofa@10453 f8a8aa39-ebdb-4476-bae3-08f188f1b544

--- applications/sofa/gui/qt/GenGraphForm.cpp
+++ applications/sofa/gui/qt/GenGraphForm.cpp
@@ -339,7 +339,7 @@ void GenGraphForm::doExport()
         addTask(argv);
         if (!exp)
         {
-            exp = true;
+            //exp = true;
             exportedFile = basefile+".svg";
         }
     }
