77847a4901f6bc29b554ab018bbddf6e1d5188d0
Juan Pablo de la Plata Alcalde <juanpablo.delaplata@insimo.fr>
2015-04-27 17:30:15 +0200

    UPDATE: set ForcesMapped and MassesMapped to false in the mappings of the contacts based on constraints

--- modules/SofaMeshCollision/IdentityContactMapper.h
+++ modules/SofaMeshCollision/IdentityContactMapper.h
@@ -84,6 +84,15 @@ public:
 
     MMechanicalState* createMapping(const char* name="contactPoints");
 
+    void setConstraintMode()
+    {
+        if (mapping!=NULL)
+        {
+            mapping->setForcesMapped(false);
+            mapping->setMassesMapped(false);
+        }
+    }
+
     void resize(int /*size*/)
     {
     }
@@ -151,6 +160,10 @@ public:
     {
         if (model==NULL) return NULL;
         return model->getMechanicalState();
+    }
+
+    void setConstraintMode()
+    {
     }
 
     void resize(int /*size*/)
