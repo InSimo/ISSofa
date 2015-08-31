c7bbbf5c45ca121313489299437938af159aa4fb
Marc Legendre <marc.legendre@inria.fr>
2015-08-31 14:51:26 +0200

    Avoid #defining snprintf under VS2015
    The snprintf() function didn't exist on Windows until MSVC++ 14, and
    snprintf was simply #defined as _snprintf.  This commit checks for the
    version of MSVC++ 14 before defining snprintf as a macro.
    
    Conflicts:
    	framework/sofa/helper/system/config.h

--- framework/sofa/helper/system/config.h
+++ framework/sofa/helper/system/config.h
@@ -35,7 +35,12 @@
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
-#define snprintf _snprintf
+#endif
+
+// snprintf() has been provided since MSVC++ 14 (Visual Studio 2015).  For other
+// versions, it is simply #defined to _snprintf().
+#if (defined(_MSC_VER) && _MSC_VER < 1900) || defined(_XBOX)
+    #  define snprintf _snprintf
 #endif
 #include <windows.h>
 #endif
