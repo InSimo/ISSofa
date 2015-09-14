ae773c4208d57285fffd7b09b13a88cb6b8b3f70
Jeremie Allard <jeremie.allard@insimo.fr>
2015-09-14 17:17:49 +0200

    ADD missing include SofaGeneral.h and remove deprecated component.h

--- modules/SofaConstraint/PrecomputedConstraintCorrection.h
+++ modules/SofaConstraint/PrecomputedConstraintCorrection.h
@@ -25,6 +25,8 @@
 #ifndef SOFA_CORE_COLLISION_CONTACTCORRECTION_H
 #define SOFA_CORE_COLLISION_CONTACTCORRECTION_H
 
+#include <sofa/SofaGeneral.h>
+
 #include <sofa/core/behavior/ConstraintCorrection.h>
 #include <sofa/core/objectmodel/DataFileName.h>
 
