0b46c7d2aebc2465d011dc79027590f199e39556
Jeremie Allard <jeremie.allard@insimo.fr>
2017-03-13 02:15:37 +0100

    ADD: sofa/helper/test.h with methods initBeforeTests() and cleanupAfterTests() to gather common code used for unit tests.
    Same as commit 5a20770c722d2323d9ae5bb2bca0fea278d44edf on integration branch

--- framework/framework_test/main.cpp
+++ framework/framework_test/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "gtest/gtest.h"
+#include <sofa/helper/test.h>
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
@@ -7,5 +8,9 @@ int main(int argc, char **argv) {
     // Set LC_CTYPE according to the environnement variable.
     setlocale(LC_CTYPE, "");
 
-    return RUN_ALL_TESTS();
+    sofa::helper::initBeforeTests();
+    int ret = RUN_ALL_TESTS();
+    sofa::helper::cleanupAfterTests();
+
+    return ret;
 }
