772dbe7ab10a17d20adcf1a0422fd1f74988bbd0
Gauthier Zirnhelt <gauthier.zirnhelt@insimo.fr>
2017-06-19 14:14:27 +0200

    FIX: Add missing stream operators to test structs

--- framework/framework_test/core/objectmodel/StructTypeInfoTest.cpp
+++ framework/framework_test/core/objectmodel/StructTypeInfoTest.cpp
@@ -22,26 +22,38 @@ struct DataStructTypeInfoTest: public ::testing::Test
 /// Structures to test ///
 //////////////////////////
 
-struct EmptyStruct {};
+struct EmptyStruct
+{
+    inline friend std::ostream& operator<<(std::ostream& os, const EmptyStruct& /*s*/) { return os; }
+    inline friend std::istream& operator>>(std::istream& in, EmptyStruct& /*s*/) { return in; }
+};
 struct SimpleStruct
 {
     int myInt;
     float myFloat;
     unsigned char myUChar;
     bool myBool;
+    inline friend std::ostream& operator<<(std::ostream& os, const SimpleStruct& /*s*/) { return os; }
+    inline friend std::istream& operator>>(std::istream& in, SimpleStruct& /*s*/) { return in; }
 };
 struct NestedStruct
 {
     SimpleStruct mySimpleStruct;
+    inline friend std::ostream& operator<<(std::ostream& os, const NestedStruct& /*s*/) { return os; }
+    inline friend std::istream& operator>>(std::istream& in, NestedStruct& /*s*/) { return in; }
 };
 struct ContainerStruct
 {
     std::vector<int> myIntVector;
     helper::set<float> myFloatSet;
+    inline friend std::ostream& operator<<(std::ostream& os, const ContainerStruct& /*s*/) { return os; }
+    inline friend std::istream& operator>>(std::istream& in, ContainerStruct& /*s*/) { return in; }
 };
 struct PointerStruct
 {
     double *myDoublePointer;
+    inline friend std::ostream& operator<<(std::ostream& os, const PointerStruct& /*s*/) { return os; }
+    inline friend std::istream& operator>>(std::istream& in, PointerStruct& /*s*/) { return in; }
 };
 
 using StructTypes = testing::Types<
