b99d3199da19cbfb9cfb5666c290ecaaa761114d
mcoquet <mcoquet@insimo.fr>
2017-06-20 12:03:17 +0200

    ADD: SOFA_TO_STRING test

--- framework/framework_test/helper/preprocessorTest.cpp
+++ framework/framework_test/helper/preprocessorTest.cpp
@@ -16,4 +16,12 @@ TEST(SofaPreprocessorTest, VectorSize)
     EXPECT_EQ(8, text_vector.size());
 }
 
+TEST(SofaPreprocessorTest, ToString)
+{
+    std::vector<const char*> text_vector;
+    SOFA_FOR_EACH(SOFA_PP_TEST_ADD_VECTOR, SOFA_EMPTY_DELIMITER,
+     SOFA_TO_STRING(Never, send, a, human, to, do, a, machine s, job))
+        EXPECT_EQ(9, text_vector.size());
+}
+
 }
