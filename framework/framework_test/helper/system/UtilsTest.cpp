e2a1e23f08ce2754039180969daa06d3771dc8ac
Gauthier Zirnhelt <gauthier.zirnhelt@insimo.fr>
2017-06-30 17:02:11 +0200

    FIX: use UTF-8 locale when converting chars to wide chars (but please use std::to_wstring now)

--- framework/framework_test/helper/system/UtilsTest.cpp
+++ framework/framework_test/helper/system/UtilsTest.cpp
@@ -5,6 +5,8 @@ using namespace sofa::helper::system;
 
 TEST(UtilsTest, string_to_widestring_to_string)
 {
+    char* loc = std::setlocale(LC_CTYPE, "en_US.UTF-8");
+
     std::string ascii_chars;
     for (char c = 32 ; c <= 126 ; c++)
         ascii_chars.push_back(c);
@@ -12,6 +14,8 @@ TEST(UtilsTest, string_to_widestring_to_string)
 
     const std::string s("chaîne de test avec des caractères accentués");
     EXPECT_EQ(s, Utils::ws2s(Utils::s2ws(s)));
+
+    std::setlocale(LC_CTYPE, loc);
 }
 
 TEST(UtilsTest, widestring_to_string_to_widestring)
