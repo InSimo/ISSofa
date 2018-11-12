069d01c6fda3b30c05fca8fcff5c84e6b425a485
Guillaume Peiffert <guillaume.peiffert@insimo.fr>
2018-11-12 13:56:00 +0100

    [ISSofa][SofaBaseLinearSolver] FIX: force CompressedRowSparseMatrix and SparseMatrix class instantiations (//Ref// T6012). They were previously instantiated by the solver at step 1 in a multithreaded context, by a DynamicCast calling TClass::CreateUnique function, which is not thread safe.
    (cherry picked from commit 3d21289824798ae0b836bc4b37a0d98cf5a8c893)

---
+++ modules/SofaBaseLinearSolver/SparseMatrix.cpp
@@ -0,0 +1,52 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+
+#include "SparseMatrix.h"
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace linearsolver
+{
+
+// FIXME: Quick and dirty way to ensure the class shared pointer creation (see TClass::CreateUnique) is not called in a multithreaded context
+const sofa::core::objectmodel::BaseClassInfo* SparseMatrixClasses[] = {
+#ifndef SOFA_FLOAT
+    SparseMatrix<double>::GetClass(),
+#endif
+#ifndef SOFA_DOUBLE
+    SparseMatrix<float>::GetClass(),
+#endif
+    NULL
+};
+
+} // namespace linearsolver
+
+} // namespace component
+
+} // namespace sofa
