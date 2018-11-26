ca05a12b5484653832bf0732255a87a7bb0900e5
Julien Bracquart <julien.bracquart@insimo.fr>
2018-11-26 16:34:47 +0100

    [SofaConstraint] Add dll export of ConstraintStoreLambdaVisitor symbol
    //Ref// T6389
    
    (cherry picked from commit 0b61bd7089076a7166a28ac8777984ff7ec96db4)

--- modules/SofaConstraint/ConstraintStoreLambdaVisitor.h
+++ modules/SofaConstraint/ConstraintStoreLambdaVisitor.h
@@ -2,6 +2,7 @@
 #define SOFA_CONSTRAINT_CONSTRAINTSTORELAMBDAVISITOR_H
 
 #include <sofa/simulation/common/MechanicalVisitor.h>
+#include <sofa/SofaGeneral.h>
 
 
 namespace sofa
@@ -9,7 +10,7 @@ namespace sofa
 namespace simulation
 {
 
-class ConstraintStoreLambdaVisitor : public BaseMechanicalVisitor
+class SOFA_CONSTRAINT_API ConstraintStoreLambdaVisitor : public BaseMechanicalVisitor
 {
 public:
     ConstraintStoreLambdaVisitor(const sofa::core::ConstraintParams* cParams, const sofa::defaulttype::BaseVector* lambda);
