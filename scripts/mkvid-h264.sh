c46ebb68e9ab42fcade017b334abd4494fd9bb1e
Jeremie Allard <jeremie.allard@insimo.fr>
2018-12-05 11:52:18 +0100

    [ISSofa] scripts: update mkvid-h264.sh to no allow both bmp and png as input, no longer require transcode, no longer copy/link each frame
    (cherry picked from commit 53766d68c5769e1f6ebd746c4adee81b008315c5)

--- scripts/mkvid-h264.sh
+++ scripts/mkvid-h264.sh
@@ -1,39 +1,84 @@
 #!/bin/bash
 if [ "$*" == "" ]; then
-echo "Usage: $0 output.avi fps step input_prefix bitrate [\"options\"]"
-echo "If the result is flipped vertically, add -z in options"
-echo "If the red/blue channels are swapped, add -k in options"
+echo "Usage: $0 output.avi fps step input_prefix [codec] [\"options\"]"
+echo "Possible codecs are: ffv1 huffyuv mjpeg h264[default] llh264fast llh264slow"
 exit 1
 fi
+
+PREFIX=${4:-seq}
+FIRST=""
+LAST=""
 
 shopt -s nullglob
 let i=0;
 let n=${3:-1};
-(
-for f in ${4:-seq}?.png ${4:-seq}??.png ${4:-seq}???.png ${4:-seq}????.png ${4:-seq}?????.png ${4:-seq}??????.png ${4:-seq}???????.png ${4:-seq}????????.png ${4:-seq}?.bmp ${4:-seq}??.bmp ${4:-seq}???.bmp ${4:-seq}????.bmp ${4:-seq}?????.bmp ${4:-seq}??????.bmp ${4:-seq}???????.bmp ${4:-seq}????????.bmp; do
+{
+for f in ${PREFIX}*; do
 #for f in capture*.png; do
-  if [ $(($i%$n)) -eq 0 ]; then
-      echo $f
+    if [ $(($i%$n)) -eq 0 ]; then
+        if [ -z "${FIRST}" ]; then
+            FIRST=$f
+        fi
+        LAST=$f
+        echo file $f
+        echo duration 1
   fi
   let i+=1
 done
-) > seqlist
-#find . -name "seq*.png" > seqlist
-FIRST=`head -1 seqlist`
-TAIL=`tail -1 seqlist`
-let i=0; while [ $i -lt 30 ]; do echo $TAIL >> seqlist; let i+=1; done
-RES=`identify -format %wx%h $FIRST`
-OPTS="-i seqlist -x imlist,null -H 0 -g $RES  -w ${5:-1000} -y ffmpeg,null -F h264 -f ${2:-30} -w ${5:-1000}"
-TRANSVER=`transcode --version 2>&1 | awk ' { print $2; } '`
-if [ "${TRANSVER:0:4}" == "v1.0" ]; then
-  echo Identified transcode v1.0.x
-  OPTS="$OPTS --use_rgb"
-elif [ "${TRANSVER:0:4}" == "v1.1" ]; then
-  echo Identified transcode v1.1.x
-  OPTS="$OPTS -V rgb24"
-else
-  echo Unknown transcode version $TRANSVER
+echo file $TAIL
+echo duration 30
+echo file $TAIL
+} > seqlist
+echo Found $i frames
+if [ $i -eq 0 ]; then
+    exit 1
 fi
-echo transcode $OPTS -o ${1:-video-h264.avi} $6
-transcode $OPTS -o ${1:-video-h264.avi} $6
-ls -l ${1:-video-h264.avi}
+#find . -name "seq*.bmp" > seqlist
+#FIRST=`head -1 seqlist`
+#TAIL=`tail -1 seqlist`
+#RES=`identify -format %wx%h $FIRST`
+#if [ -d seqframes ]; then rm -rf seqframes; fi
+#mkdir seqframes
+#let n=100000
+#for f in `cat < seqlist`; do ln $f seqframes/frame-${n:1:5}.bmp || ln -s ../$f seqframes/frame-${n:1:5}.bmp; let n+=1; done
+
+OPTS="-y -r ${2:-30}"
+#OPTS="$OPTS -s $RES"
+OPTS="$OPTS -f concat -i seqlist"
+
+CODEC=${5:-h264}
+case "$CODEC" in
+ffv1)
+  OPTS="$OPTS -pix_fmt yuv420p -flags +bitexact -vcodec ffv1"
+  ;;
+huffyuv)
+  OPTS="$OPTS -pix_fmt yuv420p -flags +bitexact -vcodec huffyuv"
+  ;;
+mjpeg)
+  OPTS="$OPTS -flags +bitexact -vcodec mjpeg -qmin 1 -qscale 1"
+  ;;
+h264)
+#  OPTS="$OPTS -pix_fmt yuv420p -vcodec libx264 -cqp 0 -me_method dia -subq 1 -partitions -parti4x4-parti8x8-partp4x4-partp8x8-partb8x8"
+#  OPTS="$OPTS -flags +bitexact+gray+umv+aic+aiv -vcodec libx264 -cqp 0"
+#  OPTS="$OPTS -flags +bitexact+gray+umv+aic+aiv -vcodec libx264 -vpre lossless_medium -threads 0"
+  OPTS="$OPTS -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p"
+  ;;
+llh264fast)
+  OPTS="$OPTS -flags +bitexact+gray+umv+aic+aiv -vcodec libx264 -vpre lossless_fast -threads 0"
+  ;;
+llh264slow)
+  OPTS="$OPTS -flags +bitexact+gray+umv+aic+aiv -vcodec libx264 -vpre lossless_max -threads 0"
+  ;;
+*)
+  echo "Unknown or unsupported codec $CODEC"
+  exit 1
+  ;;
+esac
+
+
+
+OPTS="$OPTS ${6} ${1:-video-lossless.avi}"
+
+echo ffmpeg $OPTS
+ffmpeg $OPTS
+ls -l ${1:-video-lossless.avi}
