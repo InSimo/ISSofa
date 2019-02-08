2c5226d630b009c8978162e400be72268e9f2d56
Geoffrey Lienhart <geoffrey.lienhart@insimo.fr>
2019-02-08 16:28:36 +0100

    [ISSofa][Modules] Migrate Sofa components libraries into plugins
    //Ref// T6788
    
    (cherry picked from commit aa61cebd22f8999f5f0ffdd9fc779a5800817706)

---
+++ modules/SofaComponentBase/initPlugin.cpp
@@ -0,0 +1,73 @@
+/*******************************************************************************
+*          Private SOFA components, (c) 2017 InSimo                            *
+* CONFIDENTIAL SOURCE CODE. This file is the property of InSimo and should not *
+* be redistributed. Commercial use is prohibited without a specific license.   *
+*******************************************************************************/
+#include "initPlugin.h"
+#include <sofa/core/ObjectFactory.h>
+
+
+namespace sofa
+{
+namespace component
+{
+//Here are just several convenient functions to help user to know what contains the plugin
+
+extern "C" {
+    SOFA_COMPONENT_BASE_API void initExternalModule();
+    SOFA_COMPONENT_BASE_API const char* getModuleName();
+    SOFA_COMPONENT_BASE_API const char* getModuleVersion();
+    SOFA_COMPONENT_BASE_API const char* getModuleLicense();
+    SOFA_COMPONENT_BASE_API const char* getModuleDescription();
+    SOFA_COMPONENT_BASE_API const char* getModuleComponentList();
+}
+
+void initExternalModule()
+{
+    static bool first = true;
+    if (first)
+    {
+        first = false;
+    }
+
+}
+
+const char* getModuleName()
+{
+    return "SofaComponentBase";
+}
+
+const char* getModuleVersion()
+{
+    return "0.0";
+}
+
+const char* getModuleLicense()
+{
+    return "LGPL";
+}
+
+
+const char* getModuleDescription()
+{
+    return "Sofa Component Base";
+}
+
+const char* getModuleComponentList()
+{
+    /// string containing the names of the classes provided by the plugin
+    static std::string classes = sofa::core::ObjectFactory::getInstance()->listClassesFromTarget(sofa_tostring(SOFA_TARGET));
+    return classes.c_str();
+}
+
+
+void initSofaComponentBase()
+{
+    initExternalModule();
+}
+
+
+} // namespace component
+
+} // namespace base
+
