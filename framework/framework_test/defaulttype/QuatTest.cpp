74ef59af6327ddaaebd61da1451690b11c3941ed
Jeremie Allard <jeremie.allard@insimo.fr>
2019-04-04 10:18:25 +0200

    [SofaFramework] ADD: unit tests for several methods of Quater.
    Including one which is currently broken (axisToQuat normalization of input axis).
    
    //Ref// T6759
    
    (cherry picked from commit 8573e4b3d224b76ced6e5471bba3a4e3595f2f5b)

--- framework/framework_test/defaulttype/QuatTest.cpp
+++ framework/framework_test/defaulttype/QuatTest.cpp
@@ -4,14 +4,21 @@
 #include <sofa/helper/system/config.h>
 #include <math.h>
 #include <fstream>
+#include <random>
+
+// How many loop iterations to run for each randomized test
+// Increase to stress test after changes in Quater.
+#define NITERATIONS 10000
 
 namespace 
 {
 
-typedef sofa::defaulttype::Quat Quat;
-typedef sofa::defaulttype::RigidCoord<3, double> RigidCoord;
-typedef sofa::defaulttype::RigidDeriv<3, double> RigidDeriv;
-typedef sofa::defaulttype::Vec3d Vec3d;
+typedef double Real;
+typedef sofa::helper::Quater<Real> Quat;
+typedef sofa::defaulttype::RigidCoord<3, Real> RigidCoord;
+typedef sofa::defaulttype::RigidDeriv<3, Real> RigidDeriv;
+typedef sofa::defaulttype::Vec<3,Real> Vec3d;
+typedef sofa::defaulttype::Mat<3,3,Real> Mat3x3;
 
 TEST(QuaternionTest, integrateRigidCompareLargeDisplacementIntegrationWithSmallIncrements)
 {
@@ -103,6 +110,219 @@ TEST(QuaternionTest, checkLogOfExponentialIsIdentity)
 
             EXPECT_NEAR(angle, angle_expected, epsilon);
             EXPECT_NEAR((axis-direction).norm(), 0, epsilon);
+        }
+    }
+}
+
+TEST(QuaternionTest, checkAxisToQuatAndRelatedMethods)
+{
+    const Real epsilon = 1e-6;
+    constexpr std::size_t nTests = NITERATIONS;
+
+    std::ranlux48_base rgen;
+    std::uniform_real_distribution<Real> rrnd(-1.0f,1.0f);
+
+    // loop many times, but stop after the first failure to not spam the log with thousands of errors
+    for (std::size_t ti = 0; ti < nTests && !::testing::Test::HasNonfatalFailure(); ++ti)
+    {
+        Vec3d axis(0, 0, 0);
+        for (std::size_t i=0; i<3; ++i)
+        {
+            axis[i] = rrnd(rgen);
+        }
+        axis.normalize();
+
+        Real angle = rrnd(rgen)*M_PI; // angles between -Pi and Pi
+
+        Quat q1;
+        q1.axisToQuat(axis, angle);
+
+        // expected properties of output quaternion
+        {
+            EXPECT_NEAR(std::fabs(q1[3]), cos(angle*0.5), epsilon);
+            EXPECT_NEAR(q1.norm(), 1.0, epsilon);
+            Vec3d rotated_axis = q1.rotate(axis);
+            EXPECT_NEAR((rotated_axis-axis).norm(), 0, epsilon);
+        }
+
+        // check that input axis does not have to be normalized
+        {
+            Real norm = pow(10.0f,rrnd(rgen)); // random norms between 0.1 and 10.0
+            Quat q2;
+            q2.axisToQuat(axis*norm, angle);
+            Quat qDiff = Quat::elementWiseDifference(q1,q2);
+            EXPECT_NEAR(qDiff.norm(), 0, epsilon);
+        }
+
+        // check that quatToAxis(axisToQuat()) results in the input values
+        {
+            Vec3d out_axis;
+            Real out_angle = 0;
+            q1.quatToAxis(out_axis, out_angle, false);
+            if (angle < 0)
+            {
+                // we check negative input angles, but in this case the output of quatToAxis
+                // should be the opposite axis
+                out_angle = -out_angle;
+                out_axis = -out_axis;
+            }
+            EXPECT_NEAR(out_angle, angle, epsilon);
+            if (std::fabs(angle) > epsilon)
+            {
+                EXPECT_NEAR(out_axis.norm(), 1.0, epsilon);
+                EXPECT_NEAR((out_axis-axis).norm(), 0, epsilon);
+            }
+        }
+
+        // check equivalence with exponentialMap(axis*angle)
+        {
+            Quat q2 = Quat::exponentialMap(axis*angle);
+            Quat qDiff = Quat::elementWiseDifference(q1,q2);
+            EXPECT_NEAR(qDiff.norm2(), 0, 1e-10);
+        }
+
+        // check that getLog() gives axis*angle
+        {
+            Vec3d log = q1.getLog();
+            EXPECT_NEAR(log.norm(), fabs(angle), epsilon);
+            EXPECT_NEAR((log-axis*angle).norm(), 0, epsilon);
+        }
+    }
+}
+
+TEST(QuaternionTest, checkPlusIsSameAsMult)
+{
+    // we have 4 operators that have different implementations but they should be equivalent
+    // (at least for normalized quaternions, and with a flipped operands order).
+    // Check that this is the case before factoring the code.
+
+    const Real epsilon = 1e-6;
+    constexpr std::size_t nTests = NITERATIONS;
+
+    std::ranlux48_base rgen;
+    std::uniform_real_distribution<Real> rrnd(-1.0f,1.0f);
+
+    // loop many times, but stop after the first failure to not spam the log with thousands of errors
+    for (std::size_t ti = 0; ti < nTests && !::testing::Test::HasNonfatalFailure(); ++ti)
+    {
+        Quat q1, q2;
+        for (std::size_t i=0; i<4; ++i)
+        {
+            q1[i] = rrnd(rgen);
+            q2[i] = rrnd(rgen);
+        }
+        q1.normalize();
+        q2.normalize();
+
+        Quat qmul = q2 * q1;
+        Quat qmuleq = q2; qmuleq *= q1;
+        Quat qadd = q1 + q2;
+        Quat qaddeq = q1; qaddeq += q2;
+        Quat qmulinv = (q1.inverse() * q2.inverse()).inverse();
+
+        // expected properties of output quaternions
+        {
+            EXPECT_NEAR(qmul.norm(), 1.0, epsilon);
+            EXPECT_NEAR(qadd.norm(), 1.0, epsilon);
+            EXPECT_NEAR(qmuleq.norm(), 1.0, epsilon);
+            EXPECT_NEAR(qaddeq.norm(), 1.0, epsilon);
+        }
+
+        // expected equivalence to applying the two quaternions in sequence
+        for (std::size_t tj = 0; tj < 5; ++tj)
+        {
+            Vec3d v0;
+            for (std::size_t i=0; i<3; ++i)
+            {
+                v0[i] = rrnd(rgen)*10.0;
+            }
+            Vec3d v1 = q1.rotate(v0);
+            Vec3d v2 = q2.rotate(v1);
+            Vec3d v12 = qmul.rotate(v0);
+            EXPECT_NEAR((v2-v12).norm(), 0.0, epsilon);
+            Vec3d v0b = qmul.inverseRotate(v12);
+            EXPECT_NEAR((v0-v0b).norm(), 0.0, epsilon);
+            Mat3x3 m;
+            qmul.toMatrix(m);
+            Vec3d v12m = m*v0;
+            EXPECT_NEAR((v12m-v12).norm(), 0.0, epsilon);
+        }
+
+        // compare qmul and qmuleq
+        {
+            EXPECT_NEAR(std::fabs(qmuleq[3]), std::fabs(qmul[3]), epsilon);
+            if (qmuleq.dot(qmul) < 0) qmuleq *= -1;
+            EXPECT_NEAR(Quat::elementWiseDifference(qmuleq,qmul).norm(), 0, epsilon);
+        }
+
+        // compare qadd and qaddeq
+        {
+            EXPECT_NEAR(std::fabs(qaddeq[3]), std::fabs(qadd[3]), epsilon);
+            if (qaddeq.dot(qadd) < 0) qaddeq *= -1;
+            EXPECT_NEAR(Quat::elementWiseDifference(qaddeq,qadd).norm(), 0, epsilon);
+        }
+
+        // compare qmul and qadd
+        {
+            EXPECT_NEAR(std::fabs(qadd[3]), std::fabs(qmul[3]), epsilon);
+            if (qadd.dot(qmul) < 0) qadd *= -1;
+            EXPECT_NEAR(Quat::elementWiseDifference(qadd,qmul).norm(), 0, epsilon);
+        }
+
+        // compare qmul and qmulinv
+        {
+            EXPECT_NEAR(std::fabs(qmulinv[3]), std::fabs(qmul[3]), epsilon);
+            if (qmulinv.dot(qmul) < 0) qmulinv *= -1;
+            EXPECT_NEAR(Quat::elementWiseDifference(qmulinv,qmul).norm(), 0, epsilon);
+        }
+    }
+}
+
+TEST(QuaternionTest, checkRotationsAndMatrices)
+{
+    const Real epsilon = 1e-6;
+    constexpr std::size_t nTests = NITERATIONS;
+
+    std::ranlux48_base rgen;
+    std::uniform_real_distribution<Real> rrnd(-1.0f,1.0f);
+
+    // loop many times, but stop after the first failure to not spam the log with thousands of errors
+    for (std::size_t ti = 0; ti < nTests && !::testing::Test::HasNonfatalFailure(); ++ti)
+    {
+        Quat q1;
+        for (std::size_t i=0; i<4; ++i)
+        {
+            q1[i] = rrnd(rgen);
+        }
+        q1.normalize();
+        Mat3x3 m;
+        Real m44[4][4];
+        float mgl[16];
+        q1.toMatrix(m);
+        q1.buildRotationMatrix(m44);
+        q1.writeOpenGlMatrix(mgl);
+        for(std::size_t i = 0; i < 3; ++i)
+        {
+            for(std::size_t j = 0; j < 3; ++j)
+            {
+                EXPECT_EQ(m[i][j], m44[i][j]);
+                EXPECT_EQ((float)m[i][j], mgl[j*4+i]);
+            }
+        }
+        for (std::size_t tj = 0; tj < 10; ++tj)
+        {
+            Vec3d v0;
+            for (std::size_t i=0; i<3; ++i)
+            {
+                v0[i] = rrnd(rgen)*10.0;
+            }
+            Vec3d v1 = q1.rotate(v0);
+            Vec3d v0b = q1.inverseRotate(v1);
+            EXPECT_NEAR((v0-v0b).norm(), 0.0, epsilon);
+            Vec3d v1m = m*v0;
+            EXPECT_NEAR((v1m-v1).norm(), 0.0, epsilon);
+            Vec3d v0m = m.multTranspose(v1);
+            EXPECT_NEAR((v0m-v0).norm(), 0.0, epsilon);
         }
     }
 }
