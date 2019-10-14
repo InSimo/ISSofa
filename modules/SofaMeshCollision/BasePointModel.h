8ac7054a7bbc20c82e2bb2cc8b18d0229fb0c495
Gauthier Zirnhelt <gauthier.zirnhelt@insimo.fr>
2019-10-14 13:41:37 +0200

    [ISSofa] Remove unused enum
    (cherry picked from commit 3d14e3517b0c0cbf2da6b262dfb9779cfbe700db)

--- modules/SofaMeshCollision/BasePointModel.h
+++ modules/SofaMeshCollision/BasePointModel.h
@@ -24,16 +24,6 @@ class SOFA_MESH_COLLISION_API BasePointModel : public sofa::core::CollisionModel
 public:
     SOFA_ABSTRACT_CLASS_EXTERNAL((BasePointModel), ((sofa::core::CollisionModel)));
 
-    enum PointFlag
-    {
-        FLAG_P1  = 1<<0, ///< Point 1  is attached to this line
-        FLAG_P2  = 1<<1, ///< Point 2  is attached to this line
-        FLAG_BP1 = 1<<2, ///< Point 1  is attached to this line and is a boundary
-        FLAG_BP2 = 1<<3, ///< Point 2  is attached to this line and is a boundary
-        FLAG_POINTS  = FLAG_P1|FLAG_P2,
-        FLAG_BPOINTS = FLAG_BP1|FLAG_BP2
-    };
-
     void init() override;
 
     void handleTopologyChange(sofa::core::topology::Topology* t) override;
